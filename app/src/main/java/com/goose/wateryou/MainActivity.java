199879f2d1a75203627172f9891cbe3b77940b83
Gustavo <GustavoSanMartin99@gmail.com>
2017-10-27 22:33:57 -0400

    Added basic networking functionality

--- app/src/main/java/com/goose/wateryou/MainActivity.java
+++ app/src/main/java/com/goose/wateryou/MainActivity.java
@@ -1,9 +1,18 @@
 package com.goose.wateryou;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ToggleButton;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
     ToggleButton waterButton = null;
@@ -19,16 +28,17 @@ public class MainActivity extends Activity {
         lampButton  = findViewById(R.id.LampToggle);
         autoButton  = findViewById(R.id.AutoToggle);
 
+
     }
 
     //when the "Water Valve" toggle is pressed
     public void onWater(View view) {
-
+        new upload().execute();
     }
 
     //when the "Lamp" button is pressed
     public void onLamp(View view) {
-
+        new upload().execute();
     }
 
     //when the "Auto" toggle is pressed
@@ -62,5 +72,41 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    private static class upload extends AsyncTask<Object, Object, Void>{
+        @Override
+        protected Void doInBackground(Object... objects) {
+            HttpURLConnection urlConnection = null;
+            String ServerUrl = "https://script.google.com/macros/s/AKfycbxtNl7nBbXJvDYXR3-MMG0_okpaXMvZeXveO9Bp1JEjOOzPJSQ/exec?pX0=3&pY0=1&pCode=codeW";
+            try {
+                URL url = new URL(ServerUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                Log.v("H", readStream(in));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                assert urlConnection != null;
+                urlConnection.disconnect();
+            }
+            return null;
+        }
+
+        //convert the returned HTML text into a string
+        private String readStream(InputStream is) {
+            try {
+                ByteArrayOutputStream bo = new ByteArrayOutputStream();
+                int i = is.read();
+                while (i != -1) {
+                    bo.write(i);
+                    i = is.read();
+                }
+                return bo.toString();
+            } catch (IOException e) {
+                return "";
+            }
+        }
     }
 }
